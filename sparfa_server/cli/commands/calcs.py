208acfebb26da4c280f9211c928c659ecc5be1d6
amanda <pandafulmanda@gmail.com>
2017-08-21 14:58:32 -0500

    sigs

--- sparfa_server/cli/commands/calcs.py
+++ sparfa_server/cli/commands/calcs.py
@@ -60,5 +60,5 @@ def calc_initial():
     """
     Calculate all queued up calculations
     """
-    all_calcs = (run_matrix_all_ecosystems_task | group(run_pe_calc_recurse_task, run_clue_calc_recurse_task))
+    all_calcs = (run_matrix_all_ecosystems_task.signature() | group(run_pe_calc_recurse_task.signature(), run_clue_calc_recurse_task.signature()))
     all_calcs.delay()
