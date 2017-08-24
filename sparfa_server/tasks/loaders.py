78acd13aa89cd87f6a215fc2d9f3baa1430b03c2
amanda <pandafulmanda@gmail.com>
2017-08-24 13:20:15 -0500

    only queue loading more course events more needed

--- sparfa_server/tasks/loaders.py
+++ sparfa_server/tasks/loaders.py
@@ -56,7 +56,9 @@ def load_courses_task(course_events_requests):
         __logs__.info('Loading courses')
         __logs__.info(course_events_requests)
         next_course_events_requests = load_courses(course_events_requests)
-        load_courses_task.apply_async(next_course_events_requests)
+
+        if len(next_course_events_requests):
+            load_courses_task.apply_async(next_course_events_requests)
 
 
 @celery.task
