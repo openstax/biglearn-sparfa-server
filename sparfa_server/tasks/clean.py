2c509435544bc551070655479a32a2c7b12be16c
Dante Soares <dante.m.soares@rice.edu>
2019-10-09 16:34:48 -0500

    Added test for the cleanup task

--- sparfa_server/tasks/clean.py
+++ sparfa_server/tasks/clean.py
@@ -12,15 +12,16 @@ def cleanup_ecosystem_matrices():
     """Calculate old unused ecosystem matrices"""
     while True:
         with transaction() as session:
-            ecosystem_matrix_uuids = session.query(EcosystemMatrix.uuid).filter(
+            ecosystem_matrices = session.query(EcosystemMatrix.uuid).filter(
                 EcosystemMatrix.superseded_by_uuid.isnot(None),
                 EcosystemMatrix.is_used_in_assignments.is_(False),
                 EcosystemMatrix.updated_at <= datetime.now() - CLEANUP_AFTER
             ).with_for_update(skip_locked=True).limit(BATCH_SIZE).all()
 
-            session.query(EcosystemMatrix).filter(
-                EcosystemMatrix.uuid.in_(ecosystem_matrix_uuids)
-            ).delete(synchronize_session=False)
+            if ecosystem_matrices:
+                session.query(EcosystemMatrix).filter(
+                    EcosystemMatrix.uuid.in_([matrix.uuid for matrix in ecosystem_matrices])
+                ).delete(synchronize_session=False)
 
-            if len(ecosystem_matrix_uuids) < BATCH_SIZE:
+            if len(ecosystem_matrices) < BATCH_SIZE:
                 break
