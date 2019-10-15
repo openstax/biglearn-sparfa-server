9c5c9243dd9e20a2ef620b97c8c1d5a3b857c367
Dante Soares <dante.m.soares@rice.edu>
2019-10-14 19:15:12 -0500

    Added Flask config and blueprint boilerplate

--- sparfa_server/__init__.py
+++ sparfa_server/__init__.py
@@ -1,6 +1,17 @@
+# https://flask.palletsprojects.com/en/1.1.x/tutorial/factory/
+from os import makedirs
+
+from flask import Flask
+
+from .api import blueprint
+
 __author__ = 'OpenStax'
 __copyright__ = 'Copyright 2017-18 Rice University'
 __license__ = 'AGPLv3'
 __version__ = '0.0.1'
 
-__all__ = ('__author__', '__copyright__', '__license__', '__version__')
+__all__ = ('__author__', '__copyright__', '__license__', '__version__', 'application')
+
+# Create and configure the app
+application = Flask(__name__, instance_relative_config=True)
+application.register_blueprint(blueprint)
