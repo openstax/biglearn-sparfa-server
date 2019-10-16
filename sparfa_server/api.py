0611c5bbe0242c9b8cb2f2efb7be2c55cd14e37f
Dante Soares <dante.m.soares@rice.edu>
2019-10-16 11:43:16 -0500

    Made API endpoint only return ecosystem_matrices with the given UUIDs Handle errors properly Set Content-Type to application/json

--- sparfa_server/api.py
+++ sparfa_server/api.py
@@ -1,18 +1,57 @@
-from flask import Blueprint, render_template
+from flask import jsonify, Blueprint, request, render_template
+from werkzeug.exceptions import BadRequest
 
 from .orm import transaction, EcosystemMatrix
 
-blueprint = Blueprint('API', __name__, template_folder='templates')
 
-@blueprint.route('/fetch_ecosystem_matrices', methods=['POST'])
+# https://flask.palletsprojects.com/en/1.1.x/patterns/apierrors/
+class ApiException(Exception):
+    status_code = 400
+    payload = {}
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        if status_code is not None:
+            self.status_code = status_code
+        if payload is not None:
+            self.payload = payload
+        self.payload['errors'] = [message]
+
+    @property
+    def response(self):
+        resp = jsonify(self.payload)
+        resp.status_code = self.status_code
+        return resp
+
+
+api_blueprint = Blueprint('API', __name__, template_folder='templates')
+
+
+@api_blueprint.route('/fetch_ecosystem_matrices', methods=['POST'])
 def show():
+    request_data = request.get_json(force=True)
+    if 'ecosystem_matrix_uuids' not in request_data:
+        raise ApiException('Request data is missing the ecosystem_matrix_uuids key')
+
+    ecosystem_matrix_uuids = request_data['ecosystem_matrix_uuids']
+    if not ecosystem_matrix_uuids:
+        return render_template(
+                   'fetch_ecosystem_matrices.json', ecosystem_matrices=[]
+               ), {'Content-Type': 'application/json'}
+
+    if len(ecosystem_matrix_uuids) > 10:
+        raise ApiException('The number of ecosystem_matrix_uuids per request is limited to 10')
+
     with transaction() as session:
-        ecosystem_matrices = session.query(EcosystemMatrix).all()
-        ecosystem_matrix_dicts = [{
-            key: value
+        ecosystem_matrices_attributes = [{
+            key if key != 'uuid' else 'ecosystem_matrix_uuid':
+                value if not hasattr(value, 'isoformat') else value.isoformat()
             for (key, value) in ecosystem_matrix.dict.items()
             if key != 'is_used_in_assignments'
-        } for ecosystem_matrix in ecosystem_matrices]
+        } for ecosystem_matrix in session.query(EcosystemMatrix).filter(
+            EcosystemMatrix.uuid.in_(ecosystem_matrix_uuids)
+        ).all()]
 
-    return render_template('fetch_ecosystem_matrices.json',
-                           ecosystem_matrices=ecosystem_matrix_dicts)
+    return render_template(
+               'fetch_ecosystem_matrices.json', ecosystem_matrices=ecosystem_matrices_attributes
+           ), {'Content-Type': 'application/json'}
--- sparfa_server/app.py
+++ sparfa_server/app.py
@@ -1,7 +1,14 @@
 from flask import Flask
 
-from .api import blueprint
+from .api import ApiException, api_blueprint
 
 # Create and configure the app
 application = Flask('BIGLEARN SPARFA SERVER', instance_relative_config=True)
-application.register_blueprint(blueprint)
+
+
+@application.errorhandler(ApiException)
+def handle_api_exception(api_exception):
+    return api_exception.response
+
+
+application.register_blueprint(api_blueprint)
